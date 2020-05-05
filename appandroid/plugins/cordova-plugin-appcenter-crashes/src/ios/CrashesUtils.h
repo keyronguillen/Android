cbf6c6a5c2a4269d834f735e8f35ad98b9335ba5
Keyron Guillén Porras <keyron.guillen@grupobabel.com>
2020-05-05 10:08:59 -0600

    Cambio de productos de pruebas

--- appandroid/plugins/cordova-plugin-appcenter-crashes/src/ios/CrashesUtils.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#import <Foundation/Foundation.h>
-
-@import AppCenterCrashes.MSErrorReport;
-
-NSDictionary* convertReportToJS(MSErrorReport* report);
-NSArray* convertReportsToJS(NSArray* reports);
